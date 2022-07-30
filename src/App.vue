ee176375c40a2f7b9f96eb50efd3bf759af5fc58
Pierre Maurice Schwang <mail@pschwang.eu>
2022-07-30 19:29:45 +0200

    feat: design changes

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,12 @@ import NavigationComponent from "@/components/NavigationComponent.vue";
   <div class="flex flex-col h-screen">
     <NavigationComponent />
     <RouterView class="grow mt-5" />
+    <footer class="bg-gray-800 shadow flex items-center justify-center p-5">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+        >© {{ new Date().getUTCFullYear() }} IntellectualSites | Made with
+        <span class="text-is-red">&hearts;</span> and
+        <a href="https://tailwindcss.com">Tailwind</a>
+      </span>
+    </footer>
   </div>
 </template>
