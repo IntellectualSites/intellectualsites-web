9c982da487fbcd8625236944014d0f2d155c06a6
Alexander Brandes <brandes.alexander@web.de>
2022-07-31 15:20:35 +0200

    chore: Satisfy ESLint (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import { vueI18n } from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
