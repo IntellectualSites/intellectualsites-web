29de52094f1816ae9a36426b4d8411182c80e58a
Alexander Brandes <mc.cache@web.de>
2022-08-14 16:37:49 +0200

    fix: Adjust config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { vueI18n } from "@intlify/vite-plugin-vue-i18n";
+import vueI18n from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
 
